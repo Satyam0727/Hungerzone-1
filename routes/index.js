cc4ef851c9570b6a70846a11f79bb96567b0905a
Prashant Kumar <prashantkumar010704@gmail.com>
2022-03-31 22:29:04 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+
+// Welcome Page
+router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
+
+// Dashboard
+router.get('/home/dashboard', ensureAuthenticated, (req, res) =>
+  res.render('home/dashboard', {
+    user: req.user
+  })
+);
+
+router.get('/home/AboutUs.ejs', ensureAuthenticated, (req, res) =>
+  res.render('home/AboutUs.ejs', {
+    user: req.user
+  })
+);
+
+router.get('/home/PhotoGallery.ejs', ensureAuthenticated, (req, res) =>
+  res.render('home/PhotoGallery.ejs', {
+    user: req.user
+  })
+);
+
+router.get('/home/Blog.ejs', ensureAuthenticated, (req, res) =>
+  res.render('home/Blog.ejs', {
+    user: req.user
+  })
+);
+
+router.get('/home/Subscription.ejs', ensureAuthenticated, (req, res) =>
+  res.render('home/Subscription.ejs', {
+    user: req.user
+  })
+);
+
+router.get('/home/Review.ejs', ensureAuthenticated, (req, res) =>
+  res.render('home/Review.ejs', {
+    user: req.user
+  })
+);
+
+module.exports = router;
